4d649e27a52458ba3093ee1bc5c136d8c5be1d57
zhouyixin <18169335369@189.cn>
2023-01-17 16:35:09 +0800

    push commit

--- src/GitTest01.java
+++ src/GitTest01.java
@@ -5,5 +5,6 @@ public class GitTest01 {
         System.out.println("3");
         System.out.println("hot-fix test");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
