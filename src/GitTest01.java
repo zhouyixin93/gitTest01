e498567a5bbf994479627941fa848e883e72910f
zhou <107289693+zhouyixin93@users.noreply.github.com>
2023-01-17 16:40:46 +0800

    Update GitTest01.java

--- src/GitTest01.java
+++ src/GitTest01.java
@@ -6,5 +6,6 @@ public class GitTest01 {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
